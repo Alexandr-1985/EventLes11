7285a8a697f289ae01e13ea322158fa130ff05df
Alexandr <alexandrgorr@gmail.com>
2021-11-08 01:41:02 +0300

    push after clean code

--- script.js
+++ script.js
@@ -1,5 +1,10 @@
 "user strict";
 
+const range = document.getElementById("range");
+const span = document.getElementById("range-span");
+const square = document.getElementById("square");
+const circle = document.getElementById("circle");
+
 /*Работать должно так: ввели в input[type=text] yellow, по нажатию на кнопку значение input[type=text] попадает в свойство style="backgroundColor: yellow" и фон квадрата должен поменяться*/
 document.getElementById("btn").addEventListener("click", function() {
     document.getElementById("square").style.backgroundColor =
@@ -10,24 +15,11 @@ document.getElementById("btn").addEventListener("click", function() {
 document.getElementById("e_btn").style.display = "none";
 
 /*при каждом изменении положения ползунка значение input[type=range] необходимо заносить в свойства ширины и высоты кружка (который внутри квадрата) (height и width) (в процентах!!) */
-const range = document.getElementById("range");
-const span = document.getElementById("range-span");
-const square = document.getElementById("square");
-const circle = document.getElementById("circle");
-
 range.addEventListener("input", function(event) {
     span.textContent = event.target.value;
 });
 
-document.getElementById("range").addEventListener("input", function() {
-    document.getElementById("circle").style.width = range.value + "%";
-    document.getElementById("circle").style.height = range.value + "%";
+range.addEventListener("input", function() {
+    circle.style.width = range.value + "%";
+    circle.style.height = range.value + "%";
 });
-
-/*or variant 
-range.addEventListner('inpup', () => {
-    const circleSize = range.value;
-    circle.style.width = circleSize + '%';
-    circle.style.height = circleSize + '%';
-})
-*/
