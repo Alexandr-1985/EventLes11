be864034170b8fb9c84b312ffa11643122df55df
Alexandr <alexandrgorr@gmail.com>
2021-11-08 01:46:02 +0300

    update

--- script.js
+++ script.js
@@ -1,18 +1,20 @@
 "user strict";
 
+const btn = document.getElementById("btn");
 const range = document.getElementById("range");
+const text = document.getElementById("text");
 const span = document.getElementById("range-span");
 const square = document.getElementById("square");
 const circle = document.getElementById("circle");
+const eBtn = document.getElementById("e_btn");
 
 /*Работать должно так: ввели в input[type=text] yellow, по нажатию на кнопку значение input[type=text] попадает в свойство style="backgroundColor: yellow" и фон квадрата должен поменяться*/
-document.getElementById("btn").addEventListener("click", function() {
-    document.getElementById("square").style.backgroundColor =
-        document.getElementById("text").value;
+btn.addEventListener("click", function() {
+    square.style.backgroundColor = text.value;
 });
 
 /* В кружке (который внутри квадрата) есть кнопка. Дать ей свойство style="display: none; "*/
-document.getElementById("e_btn").style.display = "none";
+eBtn.style.display = "none";
 
 /*при каждом изменении положения ползунка значение input[type=range] необходимо заносить в свойства ширины и высоты кружка (который внутри квадрата) (height и width) (в процентах!!) */
 range.addEventListener("input", function(event) {
